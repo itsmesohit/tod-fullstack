3b7322bba6083c5e5202d1657063faa983420ada
Sohit Kushwaha <sohitku404@gmail.com>
2024-06-04 09:15:44 +0530

    new commit

--- server/config/passport.js
+++ server/config/passport.js
@@ -3,6 +3,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
